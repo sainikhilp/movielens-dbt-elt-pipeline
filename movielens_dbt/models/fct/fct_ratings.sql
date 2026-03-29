881cbe5022cfb474505932bacdea1817b7627c53
sainikhilp <csainikhil123@gmail.com>
2026-03-29 00:15:53 -0500

    Add Fact Tables

---
+++ movielens_dbt/models/fct/fct_ratings.sql
@@ -0,0 +1,24 @@
+{{
+    config(
+        materialized = 'incremental',
+        on_schema_change = 'fail'
+    )
+}}
+
+
+WITH src_ratings AS(
+    SELECT * FROM {{ref('src_ratings')}}
+)
+SELECT 
+    user_id,
+    movie_id,
+    rating,
+    rating_timestamp
+FROM src_ratings
+WHERE rating is NOT NULL
+
+{% if is_incremental() %}
+  AND rating_timestamp > (
+      SELECT MAX(rating_timestamp) FROM {{ this }}
+  )
+{% endif %}
